ee38794bbcf87d421685645a4d87d1badb09c078
Samoeunchh <samoeunchh@gmail.com>
2023-05-03 15:22:06 +0700

    report

---
+++ HospitalManagementSystem/Controllers/PatientReportController.cs
@@ -0,0 +1,20 @@
+﻿using HospitalManagementSystem.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalManagementSystem.Controllers;
+
+public class PatientReportController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    public PatientReportController(ApplicationDbContext context)
+    {
+        _context=context;
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public async Task<JsonResult> GetData(string search)
+    => Json(await _context.Patient.Where(x=>x.PatientName.Contains(search)).ToListAsync());
+}
